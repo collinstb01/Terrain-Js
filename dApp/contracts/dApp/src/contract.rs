f5c8006ed223394d94927b549bc8800b1370f596
Evbadoloyi Collins Eguasa <collinstb01@gmail.com>
2022-09-21 12:43:01 +0100

    Building A Game

--- dApp/contracts/dApp/src/contract.rs
+++ dApp/contracts/dApp/src/contract.rs
@@ -4,7 +4,7 @@ use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response,
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg, OwnerResponse};
+use crate::msg::{OpenentResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{State, STATE};
 
 // version info for migration info
@@ -19,7 +19,7 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     let state = State {
-        count: msg.count,
+        opponent: msg.opponent,
         owner: info.sender.clone(),
     };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
@@ -27,9 +27,8 @@ pub fn instantiate(
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
-
+        .add_attribute("opponent", msg.opponent.to_string())
+        .add_attribute("owner", info.sender.clone()))
     // OK(Response::default())
 }
 
@@ -41,58 +40,23 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::Increment {} => try_increment(deps),
-        ExecuteMsg::Reset { count } => try_reset(deps, info, count),
-        ExecuteMsg::UpdateOwner { owner } => try_update(deps, owner)
+        ExecuteMsg::StartGame { opponent } => try_start_game(deps),
     }
 }
 
-fn try_update(deps: DepsMut, owner: Addr) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        state.owner = owner;
-        Ok(state)
-    });
-    Ok(Response::new().add_attribute("method", "change owner" ))
+pub fn try_start_game(deps: Deps, info: MessageInfo, opponent: String) -> Result<Response, ContractError> {
+
+    let rec: String = deps.api.addr_validate(opponent)?;
+
+    Ok(Response::new().add_attribute("method", "validate"))
 }
 
-pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        state.count += 1;
-        Ok(state)
-    })?;
-
-    Ok(Response::new().add_attribute("method", "try_increment"))
-}
-
-pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        if info.sender != state.owner {
-            return Err(ContractError::Unauthorized {});
-        }
-        state.count = count;
-        Ok(state)
-    })?;
-    Ok(Response::new().add_attribute("method", "reset"))
-}
-
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg, info: MessageInfo,) -> StdResult<Binary> {
-    match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
-        QueryMsg::GetOwner {} => to_binary(&query_owner(deps)?),
-    }
-}
-
-fn query_count(deps: Deps,) -> StdResult<CountResponse> {
-    let state = STATE.load(deps.storage)?;
-    Ok(CountResponse { count: state.count })
-}
-
-fn query_owner(deps: Deps,) -> StdResult<OwnerResponse> {
-    let state = STATE.load(deps.storage)?;
-    Ok(OwnerResponse { owner: state.owner })
-}
-
+// #[cfg_attr(not(feature = "library"), entry_point)]
+// pub fn query(deps: Deps, _env: Env, msg: QueryMsg,) -> StdResult<Binary> {
+//     match msg {
+//         QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -102,66 +66,29 @@ mod tests {
 
     #[test]
     fn proper_initialization() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
+        // let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
 
-        let info = mock_info("creator", &coins(1000, "earth"));
-        let msg = InstantiateMsg { count: 17 , owner: info.sender.clone()};
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
+        // let info = mock_info("creator", &coins(1000, "earth"));
+        // let msg = InstantiateMsg { opponent: "address".to_string() };
+        // // we can just call .unwrap() to assert this was a success
+        // let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
     }
 
     #[test]
-    fn increment() {
+    fn check_if_valid_addr() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
 
         let info = mock_info("creator", &coins(2, "token"));
-        let msg = InstantiateMsg { count: 17, owner: info };        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let info = mock_info("creator", &coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17, owner:  info };
+        let msg = InstantiateMsg { opponent: "info".to_string() };
         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
 
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+        // // beneficiary can release it
+        // let info = mock_info("anyone", &coins(2, "token"));
+        // let msg = ExecuteMsg::Increment {};
+        // let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let resOwner = query_owner(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
+        // // should increase counter by 1
     }
 
     // fn owner() {}
--- dApp/contracts/dApp/src/msg.rs
+++ dApp/contracts/dApp/src/msg.rs
@@ -1,37 +1,27 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
-    pub owner: Addr,
+    pub opponent: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg { 
-    Increment {},
-    Reset { count: i32 },
-    UpdateOwner { owner: Addr},
+    StartGame { opponent: String },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
-    GetCount {},
-    GetOwner {},
+    GetOpenent {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct OwnerResponse {
-    pub owner: Addr,
+pub struct OpenentResponse {
+    pub opponent: i32,
 }
